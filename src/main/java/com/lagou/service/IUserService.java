6f7401a690302a110e443bddf084c9a346c5cf76
xx0746 <xx0746@qq.com>
2022-05-04 22:14:50 +0800

    Initial commit

---
+++ src/main/java/com/lagou/service/IUserService.java
@@ -0,0 +1,11 @@
+package com.lagou.service;
+
+import com.lagou.utils.BeanFactory;
+
+public interface IUserService {
+
+    public void save() throws ClassNotFoundException, IllegalAccessException, InstantiationException;
+
+
+
+}
